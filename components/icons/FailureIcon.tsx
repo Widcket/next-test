04e9e79f704ee07d02671cdb79ca76eac604369c
Rita Zerrizuela <zeta@widcket.com>
2021-01-08 18:38:14 -0300

    Use React.memo on the icons

--- components/icons/FailureIcon.tsx
+++ components/icons/FailureIcon.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 
-export default function FailureIcon(): React.ReactElement {
-    return (
-        <figure className="w-1/2" title="Failure">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={1}
-                    d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-            </svg>
-        </figure>
-    );
-}
+const FailureIcon = (): React.ReactElement => (
+    <figure className="w-1/2" title="Failure">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={1}
+                d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+        </svg>
+    </figure>
+);
+
+export default React.memo(FailureIcon);
--- components/icons/LoadingIcon.tsx
+++ components/icons/LoadingIcon.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
 
-export default function LoadingIcon(): React.ReactElement {
-    return (
-        <figure className="w-1/2" title="Loading">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={1}
-                    d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z">
-                    <animateTransform
-                        attributeType="xml"
-                        attributeName="transform"
-                        type="rotate"
-                        from="0 12 12"
-                        to="360 12 12"
-                        dur="0.6s"
-                        repeatCount="indefinite"
-                    />
-                </path>
-            </svg>
-        </figure>
-    );
-}
+const LoadingIcon = (): React.ReactElement => (
+    <figure className="w-1/2" title="Loading">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={1}
+                d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z">
+                <animateTransform
+                    attributeType="xml"
+                    attributeName="transform"
+                    type="rotate"
+                    from="0 12 12"
+                    to="360 12 12"
+                    dur="0.6s"
+                    repeatCount="indefinite"
+                />
+            </path>
+        </svg>
+    </figure>
+);
+
+export default React.memo(LoadingIcon);
