429188a997fa21f618a28eb51640c63ebdd24060
Rita Zerrizuela <zeta@widcket.com>
2022-02-09 13:36:23 -0300

    Forward query params

--- components/sidebar/SidebarHeader.tsx
+++ components/sidebar/SidebarHeader.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { useUser } from '@auth0/nextjs-auth0';
 
 export default function SidebarHeader(): React.ReactElement {
+    const { query } = useRouter();
     const { user, isLoading } = useUser();
 
     const dividerClasses = 'border-l border-background-lighter';
@@ -17,7 +19,11 @@ export default function SidebarHeader(): React.ReactElement {
             {isLoading ? (
                 <p className={`${classes} ${dividerClasses}`} />
             ) : (
-                <Link href={`/api/auth/${user ? 'logout' : 'login'}`}>
+                <Link
+                    href={{
+                        pathname: `/api/auth/${user ? 'logout' : 'login'}`,
+                        query
+                    }}>
                     <p className={`${classes} ${dividerClasses}`}>{user ? 'Logout' : 'Login'}</p>
                 </Link>
             )}
